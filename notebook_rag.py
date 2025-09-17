ab31c2b4a53267110dc28a45488f15c4dd54099b
caviedesjoaquin <caviedesjoaquin@gmail.com>
2025-09-17 05:01:46 -0600

    Update notebook_rag.py

--- notebook_rag.py
+++ notebook_rag.py
@@ -4,14 +4,14 @@ from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
-from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 
-#Cargamos el TOKEN de OpenAI para poder ejecutar prompts
+# Cargamos el TOKEN de OpenAI para poder ejecutar prompts
 api_key = os.environ.get('OPENAI_API_KEY')
 
 # Paso 1: Función para cargar documentos
 # Dependiendo del tipo de archivo (pdf o txt) llama la función requerida
+
 def load_documents(file_paths):
     documents = []
     for file_path in file_paths:
@@ -40,20 +40,21 @@ def chunk_documents(documents):
 # Si la base de datos no existe, la crea; si existe, solamente la carga
 # Esto para evitar que se tenga que generar desde 0 la base de datos,
 # cada vez que se reciba un prompt del usuario
+
 def store_in_chromadb(chunks, embeddings_model):
     report = 'RetoRAG'
-    #db_path = os.environ.get("CHROMA_DATA_PATH", "./chroma")
-    db_path='./chroma_db'
+    db_path = './chroma_db'
     if not os.path.exists(db_path):
         # Create the vector store and persist it
-        vector_store = Chroma.from_documents(chunks, embeddings_model, collection_name=report,persist_directory=db_path)
+        vector_store = Chroma.from_documents(chunks, embeddings_model, collection_name=report, persist_directory=db_path)
     else:
         # Load the existing vector store
-        vector_store = Chroma(persist_directory=db_path, embedding_function=embeddings_model,collection_name=report)
+        vector_store = Chroma(persist_directory=db_path, embedding_function=embeddings_model, collection_name=report)
     return vector_store
 
 # Paso 5: Recuperar información relevante
 # Obtenemos los 5 primeros fragmentos relevantes de la base de datos
+
 def retrieve_relevant_info(question, vector_store):
     docs = vector_store.similarity_search(question, k=5)
     return docs
@@ -61,6 +62,7 @@ def retrieve_relevant_info(question, vector_store):
 # Paso 6: Generar prompt
 # Generamos el prompt usando el prompt del usuario, mas los
 # fragmentos relevantes
+
 def generate_prompt(question, relevant_docs):
     limit=3750
     # Extraemos el contexto de los documentos
@@ -92,12 +94,14 @@ def generate_prompt(question, relevant_docs):
 
 # Paso 7: Generar respuesta usando GPT-4
 # Enviamos el prompt al modelo
+
 def generate_answer(prompt):
     llm = ChatOpenAI(model_name="gpt-4", api_key=api_key, temperature=0)
     return llm.invoke(prompt)
 
 # Paso 8: Función principal
-def main(file_paths, question):
+
+def prgunta_LLM(file_paths, question):
     # Cargamos los documentos
     documents = load_documents(file_paths)
     # Fragmentamos los documentos
@@ -115,14 +119,15 @@ def main(file_paths, question):
     return answer
 
 # Porgram Principal
+
 if __name__ == "__main__":
     file_paths = ['V-GEL Información ES PDF.pdf', 'RETO_RAG.pdf']
     # Primera pregunta
     question = "¿Cuáles son las instrucciones para el alumno en la Actividad: Creación de un sistema RAG sencillo?"
-    answer = main(file_paths, question)
+    answer = pregunta_LLM(file_paths, question)
     print(f"Pregunta : {question}. \nRespuesta: {answer.content}\n\n")
     # Segunta pregunta
     question = "Cuales son las ventajas de v-gel?"
-    answer = main(file_paths, question)
+    answer = pregunta_LLM(file_paths, question)
     print(f"Pregunta : {question}. \nRespuesta: {answer.content}\n\n")
 
