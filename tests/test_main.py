aad30d6b713d655de35b5d10499495138377a1e9
caviedesjoaquin <caviedesjoaquin@gmail.com>
2025-09-17 05:28:03 -0600

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 from notebook_rag import Pregunta_LLM
 
+
 def test_main():
-  file_paths = ['V-GEL Información ES PDF.pdf', 'RETO_RAG.pdf']
-  question = "¿Cuál es el objetivo del proyecto?"
-  assert Pregunta_LLM(file_paths,question)
+    file_paths = ['V-GEL Información ES PDF.pdf', 'RETO_RAG.pdf']
+    question = "¿Cuál es el objetivo del proyecto?"
+    assert Pregunta_LLM(file_paths, question)
