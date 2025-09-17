009444f2566ba4e0b1e2e8fcdeda8d588eced0bd
caviedesjoaquin <caviedesjoaquin@gmail.com>
2025-09-16 23:56:00 -0600

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+from notebook_rag import main
+
+def test_main():
+  file_paths = ['V-GEL Información ES PDF.pdf', 'RETO_RAG.pdf']
+  question = "¿Cuál es el objetivo del proyecto?"
+  assert main(file_paths,question)
